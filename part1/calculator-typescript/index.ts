4de53ccfcf9c8e9d0a6b9e15d0b4c65824fabec8
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-03-23 14:51:10 +0100

    9.5 WebBMI Done

--- part1/calculator-typescript/index.ts
+++ part1/calculator-typescript/index.ts
@@ -1,9 +1,25 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
+import { isNotNumber } from "./notANumber";
 
 const app = express();
 
 app.get("/hello", (_, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+
+  if (isNotNumber(height) || isNotNumber(weight)) {
+    res.json({
+      error: "malformatted parameters",
+    });
+    return;
+  }
+  const bmi = calculateBmi(Number(weight), Number(height));
+
+  res.json({ height, weight, bmi });
 });
 
 app.listen("3000", () => {
